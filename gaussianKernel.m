4164d616aa71e56d892d53fa09e9d7fe8438faf9
anjapayne <anjaelisahiggins@gmail.com>
2017-09-05 12:34:28 -0700

    Auto stash before merge of "master" and "origin/master"

--- gaussianKernel.m
+++ gaussianKernel.m
@@ -1,7 +1,16 @@
+<<<<<<< Updated upstream
 function r = gaussianKernel(x,y)
 % Script to calculate the Gaussian Kernel
 % Written in 2013 by members of Stefan Leutgeb's lab
 % Last modified by Anja Payne September 5, 2017
 
 % k(u) = ((2*pi)^(-length(u)/2)) * exp(u'*u)
+=======
+function r = gaussianKernel(x,y)
+% Script to calculate the Gaussian Kernel
+% Written in 2013 by members of Stefan Leutgeb's lab
+% Last modified by Anja Payne September 5, 2017
+
+% k(u) = ((2*pi)^(-length(u)/2)) * exp(u'*u)
+>>>>>>> Stashed changes
 r = 0.15915494309190 * exp(-0.5*(x.*x + y.*y));
--- rateEstimator.m
+++ rateEstimator.m
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 function r = rateEstimator(ts,spkx,spky,x,y,invh,posx,posy,post)
 % Script to find the rate for one position in space
 % Written in 2013 by members of Stefan Leutgeb's lab
@@ -8,4 +9,16 @@ edge_corrector =  trapz(post,gaussianKernel(((posx-x)*invh),((posy-y)*invh)));
 %edge_corrector(edge_corrector<0.15) = NaN;
 r = (conv_sum / (edge_corrector + 0.1)) + 0.1; % regularised firing rate for "wellbehavedness"
 % i.e. no division by zero or log of zero
+=======
+function r = rateEstimator(ts,spkx,spky,x,y,invh,posx,posy,post)
+% Script to find the rate for one position in space
+% Written in 2013 by members of Stefan Leutgeb's lab
+% Last modified by Anja Payne September 4, 2017
+
+conv_sum = sum(gaussianKernel(((spkx-x)*invh),((spky-y)*invh)));
+edge_corrector =  trapz(post,gaussianKernel(((posx-x)*invh),((posy-y)*invh)));
+%edge_corrector(edge_corrector<0.15) = NaN;
+r = (conv_sum / (edge_corrector + 0.1)) + 0.1; % regularised firing rate for "wellbehavedness"
+% i.e. no division by zero or log of zero
+>>>>>>> Stashed changes
 % Gaussian kernel for the rate calculation
